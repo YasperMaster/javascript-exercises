da3454dc8c84ab1d9fa40e054334c3f1410d1247
Tomas Yasparra <tomasyas@gmail.com>
2024-05-17 13:37:01 -0300

    add completed exercises 01, 02 & 03. Exercise 04 function removeFromArray() still in progress

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num >= 0) {
+        let strArray = [];
+        let strRepeated = ''
+        for (i = 0; i < num; i++ ) {
+            strArray [i] = string;
+            strRepeated += string;
+        }
+        return strRepeated;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, num) {
+    for (i = 0; i < 5; i++) {
+        if (array[i] === num) {
+            array.splice(num, 0);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
